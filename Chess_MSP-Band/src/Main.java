130b51c8c54b3cbdb7c49658aca8b7dc9863e6fe
Ilie-Cristian SANDU (101689) <ilie_cristian.sandu@stud.acs.pub.ro>
2020-03-13 20:48:37 +0200

    Breakthrough

--- Chess_MSP-Band/src/Main.java
+++ Chess_MSP-Band/src/Main.java
@@ -5,179 +5,80 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void print2D(String mat[][])
-    {
+    public static void print2D(String mat[][]) {
         // Loop through all rows
         for (int i = 0; i < mat.length; i++) {
 
             // Loop through all elements of current row
-            for (int j = 0; j < mat[i].length; j++){
+            for (int j = 0; j < mat[i].length; j++) {
                 System.out.print(mat[i][j] + " ");
             }
             System.out.println();
         }
     }
 
+    public static void dummy() {
+
+    }
+
     public static void main(String[] args) throws IOException {
-        // write your code here
+
+        char[] a = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+
+        int index = 0;
         Scanner s = new Scanner(System.in);
-        System.out.println("Chouse player (1 or 2): ");
-        String mutare = s.nextLine();
-        int mutare_int=Integer.parseInt(mutare);
+        s.nextLine();
+        s.nextLine();
+        s.nextLine();
+        s.nextLine();
+        s.nextLine();
+        s.nextLine();
+        s.nextLine();
 
 
-        while ((mutare_int!=1)&&(mutare_int!=2))
-        {
-            System.out.println("Chouse player (1 or 2): ");
-            mutare = s.nextLine();
-            mutare_int=Integer.parseInt(mutare);
-        }
+        System.out.println("xboard");
+        System.out.flush();
+        System.out.println("new");
+        System.out.flush();
 
-        int player = Integer.parseInt(mutare);
-        int mutare_veche_x=0;
-        int mutare_veche_y=0;
-        int mutare_noua_x=0;
-        int mutare_noua_y=0;
+        System.out.println("feature sigint=0 sigterm=0 done=1");
+        System.out.flush();
 
-        int row=8,column=8;
+        s.nextLine();
+        while (index!=8) {
 
-        String[][] board = {  { "T","C","N","R","Q","N","C","T"},
-                { "P","P","P","P","P","P","P","P"},
-                { "0","0","0","0","0","0","0","0"},
-                { "0","0","0","0","0","0","0","0"},
-                { "0","0","0","0","0","0","0","0"},
-                { "0","0","0","0","0","0","0","0"},
-                { "p","p","p","p","p","p","p","p"},
-                { "t","c","n","r","q","n","c","t"}};
+                s.nextLine();
+            //if (s.equals("protover"))
 
-        print2D(board);
+          // System.out.println("force");
+           // System.out.flush();
+           // s.nextLine();
+            System.out.println("move "+a[index]+"7"+a[index]+"6");
+            System.out.flush();
+            index++;
+            s.nextLine();
+            s.nextLine();
 
-        try {
-            FileWriter file_iesire = new FileWriter("test.txt");
+            /*s.nextLine();
+            System.out.println("move c7c5");
+            System.out.flush();
 
-            while (mutare.compareTo("quit")!=0) {
+            s.nextLine();
+            System.out.println("edit");
+            System.out.flush();
 
-                System.out.println(mutare);
-                file_iesire.write(mutare);
-                file_iesire.write(System.getProperty( "line.separator" ));
-                mutare = s.nextLine();
+           if (s.equals("protover"))
+                System.out.println("feature sigint=0 sigterm=0 done=1");
+            s.nextLine();
+            System.out.println("move d7d6");
+            System.out.flush();
 
-                //System.out.println(mutare.charAt(0));
-                switch (mutare.charAt(0)){
-                    case 'a':
-                        mutare_veche_x=1;
-                        break;
+            s.nextLine();
+            System.out.println("move b7b6");
+            System.out.flush();
+            //index++;
 
-                    case 'b':
-                        mutare_veche_x=2;
-
-                        break;
-
-                    case 'c':
-                        mutare_veche_x=3;
-
-                        break;
-                    case 'd':
-                        mutare_veche_x=4;
-
-                        break;
-                    case 'e':
-
-                        mutare_veche_x=5;
-                        break;
-                    case 'f':
-
-                        mutare_veche_x=6;
-                        break;
-                    case 'g':
-
-                        mutare_veche_x=7;
-                        break;
-
-                    case 'h':
-
-                        mutare_veche_x=8;
-                        break;
-                    default:
-                        System.out.println("Mutare invalida swich");
-                }
-                mutare_veche_y=Character.getNumericValue(mutare.charAt(1));
-
-                switch (mutare.charAt(2)){
-                    case 'a':
-                        mutare_noua_x=1;
-                        break;
-
-                    case 'b':
-                        mutare_noua_x=2;
-
-                        break;
-
-                    case 'c':
-
-                        mutare_noua_x=3;
-                        break;
-                    case 'd':
-
-                        mutare_noua_x=4;
-                        break;
-                    case 'e':
-
-                        mutare_noua_x=5;
-                        break;
-                    case 'f':
-
-                        mutare_noua_x=6;
-                        break;
-                    case 'g':
-                        mutare_noua_x=7;
-                        break;
-
-                    case 'h':
-                        mutare_noua_x=8;
-                        break;
-                    default:
-
-                }
-                mutare_noua_y=Character.getNumericValue(mutare.charAt(3));
-
-
-                if (player==2) {
-                    if ((mutare_veche_y !=8)&&(mutare_veche_y !=7))
-                    {
-                        System.out.println("Mutare Invalida");
-                    }else
-                    {
-                        System.out.println(board[mutare_noua_y][mutare_noua_x]);
-                        System.out.println(board[mutare_noua_y][mutare_noua_x]);
-                        board[mutare_noua_y][mutare_noua_x]=board[mutare_noua_y][mutare_noua_x];
-                        board[mutare_noua_y][mutare_noua_x]="0";
-
-                    }
-                }else
-                {
-                    if ((mutare_veche_y !=1)&&(mutare_veche_y !=2))
-                    {
-                        System.out.println("Mutare Invalida");
-                    }else
-                    {
-                        board[mutare_noua_y][mutare_noua_x]=board[mutare_noua_y][mutare_noua_x];
-                        board[mutare_noua_y][mutare_noua_x]="0";
-
-                    }
-                }
-                print2D(board);
-
-
-            }
-
-
-            file_iesire.close();
-
-
-        } catch (IOException e)
-        {
-            e.printStackTrace();
+             */
         }
     }
 }
