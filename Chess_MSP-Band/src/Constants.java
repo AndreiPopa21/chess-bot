e9da88cc38dab8319c74d45223b684fef13235e7
Stefan <stefan_andrei.popa@stud.acs.upb.ro>
2020-04-12 11:32:34 +0300

    create Constants.java for holding game constants

---
+++ Chess_MSP-Band/src/Constants.java
@@ -0,0 +1,94 @@
+public final class Constants{
+    private Constants(){
+        // do nothing
+    }
+
+    public static final int A1 = 971;
+    public static final int A2 = 972;
+    public static final int A3 = 973;
+    public static final int A4 = 974;
+    public static final int A5 = 975;
+    public static final int A6 = 976;
+    public static final int A7 = 977;
+    public static final int A8 = 978;
+
+    public static final int B1 = 981;
+    public static final int B2 = 982;
+    public static final int B3 = 983;
+    public static final int B4 = 984;
+    public static final int B5 = 985;
+    public static final int B6 = 986;
+    public static final int B7 = 987;
+    public static final int B8 = 988;
+
+    public static final int C1 = 991;
+    public static final int C2 = 992;
+    public static final int C3 = 993;
+    public static final int C4 = 994;
+    public static final int C5 = 995;
+    public static final int C6 = 996;
+    public static final int C7 = 997;
+    public static final int C8 = 998;
+
+    public static final int D1 = 1001;
+    public static final int D2 = 1002;
+    public static final int D3 = 1003;
+    public static final int D4 = 1004;
+    public static final int D5 = 1005;
+    public static final int D6 = 1006;
+    public static final int D7 = 1007;
+    public static final int D8 = 1008;
+
+    public static final int E1 = 1101;
+    public static final int E2 = 1102;
+    public static final int E3 = 1103;
+    public static final int E4 = 1104;
+    public static final int E5 = 1105;
+    public static final int E6 = 1106;
+    public static final int E7 = 1107;
+    public static final int E8 = 1108;
+
+    public static final int F1 = 1201;
+    public static final int F2 = 1202;
+    public static final int F3 = 1203;
+    public static final int F4 = 1204;
+    public static final int F5 = 1205;
+    public static final int F6 = 1206;
+    public static final int F7 = 1207;
+    public static final int F8 = 1208;
+
+    public static final int G1 = 1301;
+    public static final int G2 = 1302;
+    public static final int G3 = 1303;
+    public static final int G4 = 1304;
+    public static final int G5 = 1305;
+    public static final int G6 = 1306;
+    public static final int G7 = 1307;
+    public static final int G8 = 1308;
+
+    public static final int H1 = 1401;
+    public static final int H2 = 1402;
+    public static final int H3 = 1403;
+    public static final int H4 = 1404;
+    public static final int H5 = 1405;
+    public static final int H6 = 1406;
+    public static final int H7 = 1407;
+    public static final int H8 = 1408;
+
+
+    public static final int PAWN_VALUE = 10;
+    public static final int KNIGHT_VALUE = 30;
+    public static final int BISHOP_VALUE = 30;
+    public static final int ROOK_VALUE = 50;
+    public static final int QUEEN_VALUE = 90;
+    public static final int KING_VALUE = 900;
+
+
+    public static final int WHITE_COLOR = 1;
+    public static final int BLACK_COLOR = 2;
+    public static final int NON_COLOR = -1;
+
+
+    
+
+}
