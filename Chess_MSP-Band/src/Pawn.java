30523e664c31e50818eee12d475933b353a2250e
Stefan <stefan_andrei.popa@stud.acs.upb.ro>
2020-04-13 22:09:03 +0300

    implement basic Pawn move searcher

--- Chess_MSP-Band/src/Pawn.java
+++ Chess_MSP-Band/src/Pawn.java
@@ -12,7 +12,50 @@ public class Pawn extends Piece {
 
     public ArrayList<Move> searchMoves(int src){
         ArrayList<Move> moves = new ArrayList<>();
-        return moves;
+
+        if(this.getTable().isKingChecked()){
+            return moves;
+        }
+
+        int front = src + 1;
+        int attackLeft = src - 9;
+        int attackRight = src + 11;
+
+        Square frontSq = getTable().getSquares().get(front);
+        Square leftSq = getTable().getSquares().get(attackLeft);
+        Square rightSq = getTable().getSquares().get(attackRight);
+
+        if(frontSq != null){
+            if(!frontSq.hasPiece()){
+                moves.add(new Move(src,front,null));
+            }
+        }
+        if(leftSq != null){
+            if(leftSq.hasPiece()){
+                if(leftSq.getPiece().getColor() != this.getColor()){
+                    moves.add(new Move(src,attackLeft,null));
+                }
+            }
+        }
+
+        if(leftSq != null){
+            if(leftSq.hasPiece()){
+                if(leftSq.getPiece().getColor() != this.getColor()){
+                    moves.add(new Move(src,attackLeft,null));
+                }
+            }
+        }
+
+        ArrayList<Move> finalMoves = new ArrayList<>();
+
+        for(int i = 0; i < moves.size(); i++){
+            Move move = moves.get(i);
+            if(!getTable().isKingBinded(move)){
+                finalMoves.add(move);
+            }
+        }
+
+        return finalMoves;
     }
 
     public boolean isChecking(int src, int king){
