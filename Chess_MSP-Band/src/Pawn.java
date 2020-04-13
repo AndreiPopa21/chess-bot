e8c65b700fdee181d438c9917eaf2499e30441ef
Stefan <stefan_andrei.popa@stud.acs.upb.ro>
2020-04-13 21:44:47 +0300

    refactor Pawn, still not fully implemented

--- Chess_MSP-Band/src/Pawn.java
+++ Chess_MSP-Band/src/Pawn.java
@@ -11,83 +11,18 @@ public class Pawn extends Piece {
 
 
     public ArrayList<Move> searchMoves(int src){
-        return null;
+        ArrayList<Move> moves = new ArrayList<>();
+        return moves;
     }
 
     public boolean isChecking(int src, int king){
+        int attackRight = src + 11;
+        int attackLeft = src - 9;
+
+        if(king == attackLeft) return true;
+        if(king == attackRight) return true;
+
         return false;
     }
-
-    /*
-    @Override
-    public ArrayList<String> getAllPossibleMoves() {
-        if(this.isCaptured())
-            return null;
-
-        ArrayList<String> moves = new ArrayList<>();
-        Position currPosition = this.getPosition();
-        int column = Math.abs((int) currPosition.letter - 104); //get matrix row index
-        int row = currPosition.digit-1;
-
-        if(this.getColor() == Color.WHITE) {
-            Piece p = getTable().getConfiguration()[row + 1][column];
-            if (p.getName().equals('-'))
-                moves.add(Table.generateMoveCommand(this.getPosition(), 1, 0));
-
-            int[] attackRowOff = {1, 1};
-            int[] attackColOff = {1, -1};
-            for (int i = 0; i < attackRowOff.length; i++) {
-
-                int nextRow = row + attackRowOff[i];
-                int nextCol = column + attackColOff[i];
-                if(nextRow < 0 || nextRow >=8 || nextCol < 0 || nextCol >= 8)
-                    continue;
-
-                if (getTable().getConfiguration()[nextRow][nextCol]
-                        .getColor() == Color.BLACK)
-                    moves.add(Table.generateMoveCommand(this.getPosition(),
-                            attackRowOff[i], attackColOff[i]));
-
-            }
-        }
-
-        if(this.getColor() == Color.BLACK){
-            Piece p = getTable().getConfiguration()[row-1][column];
-            if(p.getName().equals('-'))
-            moves.add(Table.generateMoveCommand(this.getPosition(),-1,0));
-
-            int[] attackRowOff = {-1,-1};
-            int[] attackColOff = {1,-1};
-            for(int i = 0; i < attackRowOff.length; i++){
-                int nextRow = row + attackRowOff[i];
-                int nextCol = column + attackColOff[i];
-                if(nextRow < 0 || nextRow >=8 || nextCol < 0 || nextCol >= 8)
-                    continue;
-                if(getTable().getConfiguration()[row+attackRowOff[i]][column+attackColOff[i]]
-                        .getColor() == Color.WHITE)
-                    moves.add(Table.generateMoveCommand(this.getPosition(),
-                            attackRowOff[i],attackColOff[i]));
-            }
-
-        }
-        if(this.getPawnState() == PAWN_STATE.NOT_ENPASSANT){
-            if(this.getColor() == Color.WHITE){
-                Piece p = getTable().getConfiguration()[row+2][column];
-                if(p.getName().equals('-'))
-                    moves.add(Table.generateMoveCommand(this.getPosition(),2, 0));
-            }
-            if(this.getColor() == Color.BLACK){
-                Piece p = getTable().getConfiguration()[row-2][column];
-                if(p.getName().equals('-'))
-                    moves.add(Table.generateMoveCommand(this.getPosition(),-2,0));
-            }
-        }
-
-
-
-
-        return moves;
-    }*/
-
 
 }
